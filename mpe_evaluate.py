5282531a5c38cb0ccf928f7ff4aa5fc89b7ba0b3
Altria <altriaex86@gmail.com>
2019-08-04 20:58:03 -0400

    add msqrppo in mpe_evaluate

--- mpe_evaluate.py
+++ mpe_evaluate.py
@@ -7,8 +7,46 @@ import os
 import maddpg.common.tf_util as U
 from maddpg.trainer.maddpg import MADDPGAgentTrainer
 import tensorflow.contrib.layers as layers
-from train import get_trainers, make_env
 from tqdm import tqdm
+from algorithms.MSQRPPO import MSQRPPO
+
+def mlp_model(input, num_outputs, scope, reuse=False, num_units=64, rnn_cell=None):
+    # This model takes as input an observation and returns values of all actions
+    with tf.variable_scope(scope, reuse=reuse):
+        out = input
+        out = layers.fully_connected(out, num_outputs=num_units, activation_fn=tf.nn.relu)
+        out = layers.fully_connected(out, num_outputs=num_units, activation_fn=tf.nn.relu)
+        out = layers.fully_connected(out, num_outputs=num_outputs, activation_fn=None)
+        return out
+
+def make_env(scenario_name, arglist, benchmark=False):
+    from multiagent.environment import MultiAgentEnv
+    import multiagent.scenarios as scenarios
+
+    # load scenario from script
+    scenario = scenarios.load(scenario_name + ".py").Scenario()
+    # create world
+    world = scenario.make_world()
+    # create multiagent environment
+    if benchmark:
+        env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, scenario.benchmark_data)
+    else:
+        env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation)
+    return env
+
+def get_trainers(env, num_adversaries, obs_shape_n, arglist):
+    trainers = []
+    model = mlp_model
+    trainer = MADDPGAgentTrainer
+    for i in range(num_adversaries):
+        trainers.append(trainer(
+            "agent_%d" % i, model, obs_shape_n, env.action_space, i, arglist,
+            local_q_func=(arglist.adv_policy=='ddpg')))
+    for i in range(num_adversaries, env.n):
+        trainers.append(trainer(
+            "agent_%d" % i, model, obs_shape_n, env.action_space, i, arglist,
+            local_q_func=(arglist.good_policy=='ddpg')))
+    return trainers
 
 class ImportMaddpgAgents():
     def __init__(self, model_path, env):
@@ -31,6 +69,36 @@ class ImportMaddpgAgents():
     def close_sess(self):
         self.sess.close()
 
+class ImportMQRPPOAgent():
+    def __init__(self, model_path, env):
+        self.graph = tf.Graph()
+        self.config = tf.ConfigProto()
+        self.config.intra_op_parallelism_threads = 1
+        self.config.inter_op_parallelism_threads= 1
+        self.sess = tf.Session(graph=self.graph, config=self.config)
+
+        agent = MSQRPPO(env, hidden_dims=[64, 64],
+                                    kappa=1e-2,
+                                    gamma=0.95,
+                                    quantile=0.5)
+        with self.graph.as_default():
+            self.agent = MSQRPPO(env, hidden_dims=[64, 64],
+                                    kappa=1e-2,
+                                    gamma=0.95,
+                                    quantile=0.5)
+            saver = tf.train.Saver()
+            saver.restore(self.sess, os.path.join(model_path, 'model.ckpt'))
+            self.agent.load_state_filter(os.path.join(model_path, 'filter.npy'))
+    def act(self, obs_n):
+        action = []
+        with self.graph.as_default():
+            with self.sess.as_default() as sess:
+                for agent in range(self.agent.n_agent):
+                    action.append(self.agent.get_deterministic_action(sess, np.array(obs_n), agent))
+        return action
+    def close_sess(self):
+        self.sess.close()
+
 def parse_args():
     parser = argparse.ArgumentParser("Reinforcement Learning experiments for multiagent environments")
     # Environment
@@ -45,7 +113,7 @@ def parse_args():
     parser.add_argument("--lr", type=float, default=1e-2, help="learning rate for Adam optimizer")
     parser.add_argument("--gamma", type=float, default=0.95, help="discount factor")
     parser.add_argument("--batch-size", type=int, default=1024, help="number of episodes to optimize at the same time")
-    parser.add_argument("--num-units", type=int, default=64, help="number of units in the mlp")
+    parser.add_argument("--num-units", type=int, default=128, help="number of units in the mlp")
     # Checkpointing
     parser.add_argument("--exp-name", type=str, default="reproduce", help="name of the experiment")
     parser.add_argument("--save-rate", type=int, default=1000, help="save model once every time this many episodes are completed")
@@ -59,26 +127,17 @@ def parse_args():
 
 if __name__ == '__main__':
     arglist = parse_args()
-    env =  make_env("simple_spread", arglist, True)
-
-    maddpg_team0_dir = os.path.join("exp-0", "simple_spread", "policy")
-    maddpg_team_0 = ImportMaddpgAgents(maddpg_team0_dir, env)
-
-    maddpg_team1_dir = os.path.join("exp-1", "simple_spread", "policy")
-    maddpg_team_1 = ImportMaddpgAgents(maddpg_team1_dir, env)
-
-    maddpg_team2_dir = os.path.join("exp-2", "simple_spread", "policy")
-    maddpg_team_2 = ImportMaddpgAgents(maddpg_team0_dir, env)
+    env =  make_env("simple_spread_modified", arglist, True)
+    team0_dir = os.path.join("exp-0", "MSQRPPO_simple_spread_modified")
+    team_0 = ImportMQRPPOAgent(team0_dir, env)
     
     infos = []
-    n_epi = 4000
+    n_epi = 400
     for epi in tqdm(range(n_epi), ncols=100):
+        obs_n = env.reset()
         for step in range(25):
-            obs_n = env.reset()
-            action_0 = maddpg_team_0.act(obs_n)
-            action_1 = maddpg_team_1.act(obs_n)
-            action_2 = maddpg_team_2.act(obs_n)
-            action = [action_1[0], action_1[1], action_1[2]]
+            action_0 = team_0.act(obs_n)
+            action = [action_0[0], action_0[1], action_0[2]]
             obs_n, rew_n, done_n, info_n = env.step(action)
             infos.append(info_n['n'])
             if all(done_n):
@@ -88,6 +147,4 @@ if __name__ == '__main__':
         for info_ in info:
             occ.append(info_[3])
     print(sum(occ)/(len(occ)))
-    maddpg_team_0.close_sess()
-    maddpg_team_1.close_sess()
-    maddpg_team_2.close_sess()
+    team_0.close_sess()
